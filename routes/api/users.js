99ad8a9e52788e82d2d38d37802b55dbfd188bba
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-12-03 20:53:47 +0100

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,10 +5,12 @@ const { users: ctrl } = require('../../controllers');
 const { joiSignupSchema, joiLoginSchema } = require('../../models/user');
 
 const router = express.Router();
-
-router.post('/signup', validation(joiSignupSchema), ctrlWrapper(ctrl.signup));
 // "/register"
+router.post('/signup', validation(joiSignupSchema), ctrlWrapper(ctrl.signup));
+
 router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+
+router.get('/logout', auth, ctrlWrapper(ctrl.logout));
 
 router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
 
